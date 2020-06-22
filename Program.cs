685f07ec6db225b4c2630fb99c01ef31a402eb44
David Gillis <dgillis91@gmail.com>
2020-06-22 18:43:06 -0500

    write test harness

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using CustomCollections;
 
 namespace BPSim {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[] data = makeLinearIntegerArray(10);
+            Heap<int> heap = new Heap<int>(data);
+            
+            Console.WriteLine("Initial Heap");
+            Console.WriteLine(heap);
+
+            Console.WriteLine("First Extract: {0}", heap.Extract());
+            Console.WriteLine(heap);
+
+            Console.WriteLine("Second Extract: {0}", heap.Extract());
+            Console.WriteLine(heap);
+        }
+
+        private static int[] makeLinearIntegerArray(int length) {
+            int[] array = new int[length];
+            for (int i = 0; i < length; ++i) {
+                array[i] = i;
+            }
+            return array;
         }
     }
 }
